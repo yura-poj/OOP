c30423ed828191a691057205bafa02a0302fb33d
Yuriy Pozhidaev <y.pozhidaev@g.nsu.ru>
2024-09-05 12:30:25 +0700

    sample

---
+++ Task_1_1_1/src/test/java/ru/nsu/pozhidaev/MainTest.java
@@ -0,0 +1,11 @@
+package ru.nsu.pozhidaev;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+    @Test
+    void SampleTest(){
+        assertEquals(3,Main.sum(1,2));
+    }
+}
