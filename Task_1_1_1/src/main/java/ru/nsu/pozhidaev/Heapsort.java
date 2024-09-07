818c83d3b6c63d6253141293136afa20aa9f4892
Yuriy Pozhidaev <y.pozhidaev@g.nsu.ru>
2024-09-07 18:42:41 +0700

    add points

--- Task_1_1_1/src/main/java/ru/nsu/pozhidaev/Heapsort.java
+++ Task_1_1_1/src/main/java/ru/nsu/pozhidaev/Heapsort.java
@@ -1,16 +1,16 @@
 package ru.nsu.pozhidaev;
 
 /**
- * The main class
+ * The main class.
  **/
 public class Heapsort {
 
     /**
-     * swap the elements of array with indexes a and b
+     * swap the elements of array with indexes a and b.
      *
-     * @param arr array
-     * @param a   index of first element
-     * @param b   index of second element
+     * @param arr array.
+     * @param a   index of first element.
+     * @param b   index of second element.
      **/
     private static void swap(int[] arr, int a, int b) {
         int temp = arr[a];
@@ -19,11 +19,11 @@ public class Heapsort {
     }
 
     /**
-     * sift probably small element to the right position
+     * sift probably small element to the right position.
      *
-     * @param arr  array
-     * @param n    the max possible index
-     * @param head index of number which need to sift
+     * @param arr  array.
+     * @param n    the max possible index.
+     * @param head index of number which need to sift.
      **/
     private static void siftup(int[] arr, int n, int head) {
         int largest = head;
@@ -44,30 +44,30 @@ public class Heapsort {
     }
 
     /**
-     * heapsort is a type of sorting algorithm
-     * which use binary tree where on the top is the largest element
-     * on each step we delete this largest element and add it to the new sorted array
+     * heapsort is a type of sorting algorithm.
+     * which use binary tree where on the top is the largest element.
+     * on each step we delete this largest element and add it to the new sorted array.
      *
-     * @param arr array needed to be sorted
-     * @return sorted array
+     * @param arr array needed to be sorted.
+     * @return sorted array.
      **/
     public static int[] heapsort(int[] arr) {
-        // Practically this cycle usually create in other function by name heapify
+        // Practically this cycle usually create in other function by name heapify.
         for (int i = arr.length / 2 - 1; i >= 0; i--) {
-            siftup(arr, arr.length, i); // We are create heap in the right order
+            siftup(arr, arr.length, i); // We are create heap in the right order.
         }
 
         for (int i = arr.length - 1; i >= 0; i--) {
-            swap(arr, 0, i); // swap the largest element with ith element
-            siftup(arr, i, 0); // sift this element to avoid smaller number was upper than bigger
+            swap(arr, 0, i); // swap the largest element with ith element.
+            siftup(arr, i, 0); // sift this element to avoid smaller number was upper than bigger.
         }
-        return arr; // return sorted array
+        return arr; // return sorted array.
     }
 
     /**
-     * do nothing
+     * do nothing.
      *
-     * @param args arguments of command line
+     * @param args arguments of command line.
      **/
     public static void main(String[] args) {
     }
