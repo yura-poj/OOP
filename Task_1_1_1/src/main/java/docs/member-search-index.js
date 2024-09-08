03d5b5fa1bde0939065a964d415fcecb792a3a83
Yuriy Pozhidaev <y.pozhidaev@g.nsu.ru>
2024-09-08 14:56:34 +0700

    add javadoc to script

---
+++ Task_1_1_1/src/main/java/docs/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"ru.nsu.pozhidaev","c":"Heapsort","l":"Heapsort()","u":"%3Cinit%3E()"},{"p":"ru.nsu.pozhidaev","c":"Heapsort","l":"heapsort(int[])"},{"p":"ru.nsu.pozhidaev","c":"Heapsort","l":"main(String[])","u":"main(java.lang.String[])"}];updateSearchResults();
--- Task_1_1_1/src/main/java/run.sh
+++ Task_1_1_1/src/main/java/run.sh
@@ -1,10 +1,8 @@
 : << 'COMMENT'
-This script compile Main.java into the Main.class
-after that we combine it into jar file Main.jar and run it
-to avoid extra files we delete them
+This script compile Heapsort.java into the Heapsort.class
+after that we combine it into jar file Heapsort.jar and run it
 COMMENT
-javac ./ru/nsu/pozhidaev/Main.java #compile Main.java into Main.class
-jar -cfe Main.jar ru.nsu.pozhidaev.Main ru/nsu/pozhidaev/Main.class # combine Main.class into Main.jar
-java -jar Main.jar # run Main.jur file
-rm ru/nsu/pozhidaev/Main.class # delete extra file Main.class after running the program
-rm Main.jar # delete extra file Main.jar after running the program
+#javadoc -d docs ./ru/nsu/pozhidaev/Heapsort.java
+javac ./ru/nsu/pozhidaev/Heapsort.java #compile Heapsort.java into Heapsort.class
+jar -cfe Heapsort.jar ru.nsu.pozhidaev.Heapsort ru/nsu/pozhidaev/Heapsort.class # combine Heapsort.class into Heapsort.jar
+java -jar Heapsort.jar # run Heapsort.jur file
