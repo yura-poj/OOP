1b3cb2129125c45e6f02b96b6697a250bd4de862
yura-poj <yura-poj@users.noreply.github.com>
2024-09-27 06:01:37 +0000

    Deploying to gh-pages from @ yura-poj/OOP@c818f0e6e0f6ecb70ad0893d885e725d0323150f 🚀

--- Task_1_2_2/type-search-index.js
+++ Task_1_2_2/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"ru.nsu.pozhidaev","l":"HashTable"}]
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"ru.nsu.pozhidaev","l":"HashTable"},{"p":"ru.nsu.pozhidaev","l":"Structure"}]
