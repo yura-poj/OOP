5f96cc3501a7a38272b07492d599dc9b40f0fda4
Yurii Pozhidaev <y.pozhidaev@g.nsu.ru>
2024-10-01 22:54:31 +0700

    checkstyle and tests

--- Task_1_3_1/src/main/java/ru/nsu/pozhidaev/SearchSubstring.java
+++ Task_1_3_1/src/main/java/ru/nsu/pozhidaev/SearchSubstring.java
@@ -6,24 +6,35 @@ import java.io.IOException;
 import java.io.Reader;
 import java.util.ArrayList;
 
+/** search substring from class Reader or string. */
 public class SearchSubstring {
     private ArrayList<Integer> results;
 
+    /** constructor. */
     public SearchSubstring() {
         results = new ArrayList<>();
     }
 
+    /**
+     * search substring. create buffer with length of param text, and compare it with substring of
+     * size of param text in file. after each iteration remove first element in buffer and add next.
+     *
+     * @param reader subclass of Reader which will be called in function to get chars.
+     * @param text you want to find in file.
+     * @return array list with indexes of first letters of substring.
+     * @throws IOException Signals that an I/O exception to some sort has occurred.
+     */
     public ArrayList<Integer> search(Reader reader, String text) throws IOException {
         int startSubstringIndex = 0;
         char currentChar = 0;
         ArrayList<Character> buffer = new ArrayList<>();
-        for(int j = 0; j < text.length(); j++){
+        for (int j = 0; j < text.length(); j++) {
             currentChar = newChar(reader);
             buffer.add(currentChar);
         }
         buffer.toArray();
-        while(currentChar != '\uFFFF'){
-            if(compareCharArray(buffer, text)){
+        while (currentChar != '\uFFFF') {
+            if (compareCharArray(buffer, text)) {
                 results.add(startSubstringIndex);
             }
 
@@ -36,35 +47,55 @@ public class SearchSubstring {
         return results;
     }
 
+    /**
+     * overloaded method that open file reader by string and call another search.
+     *
+     * @param reader string of name of file which will be opened.
+     * @param text substring that should be found.
+     * @return array list with indexes of first letters of substring.
+     * @throws IOException Signals that an I/O exception to some sort has occurred.
+     */
     public ArrayList<Integer> search(String reader, String text) throws IOException {
         BufferedReader bufferedReader = null;
         try {
             bufferedReader = new BufferedReader(new FileReader(reader));
-        }
-        catch(IOException e){
+        } catch (IOException e) {
             System.out.println(e.getMessage());
         }
 
         return search(bufferedReader, text);
     }
 
-    private boolean compareCharArray(ArrayList<Character> buffer, String text){
-        for(int i = 0; i < text.length(); i++){
-            if(buffer.get(i) != text.charAt(i)){
+    /**
+     * compare array list of chars and string.
+     *
+     * @param buffer array list of chars.
+     * @param text string to compare.
+     * @return true if array equal string, and false in other case.
+     */
+    private boolean compareCharArray(ArrayList<Character> buffer, String text) {
+        for (int i = 0; i < text.length(); i++) {
+            if (buffer.get(i) != text.charAt(i)) {
                 return false;
             }
         }
         return true;
     }
 
+    /**
+     * get new char from Reader and remove extra values like '\n' '\r' etc.
+     *
+     * @param reader from which will getting chars.
+     * @return new char.
+     * @throws IOException Signals that an I/O exception to some sort has occurred.
+     */
     private Character newChar(Reader reader) throws IOException {
         char result = '0';
-        while(true){
+        while (true) {
             result = (char) reader.read();
-            if(result != '\n' && result != '\r'){
+            if (result != '\n' && result != '\r') {
                 return result;
             }
         }
-
     }
 }
--- Task_1_3_1/src/test/java/ru/nsu/pozhidaev/SearchSubstringTest.java
+++ Task_1_3_1/src/test/java/ru/nsu/pozhidaev/SearchSubstringTest.java
@@ -1,27 +1,32 @@
 package ru.nsu.pozhidaev;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.io.*;
-import java.util.ArrayList;
-
-import static org.junit.jupiter.api.Assertions.*;
-
 class SearchSubstringTest {
     SearchSubstring searchSubstring;
+
     @BeforeEach
     void setUp() {
         searchSubstring = new SearchSubstring();
     }
+
     @Test
     void searchFileName() throws IOException {
         ArrayList<Integer> list = new ArrayList<>();
         list.add(0);
         list.add(10);
 
-        ArrayList<Integer> list1 =  searchSubstring.search(
-                "./src/test/resources/test.txt", "aboba");
+        ArrayList<Integer> list1 = searchSubstring.search("./src/test/resources/test.txt", "aboba");
         assertEquals(list, list1);
     }
 
@@ -30,24 +35,43 @@ class SearchSubstringTest {
         ArrayList<Integer> list = new ArrayList<>();
         list.add(0);
         list.add(10);
-        FileReader fileReader =  new FileReader("./src/test/resources/test.txt");
-        ArrayList<Integer> list1 =  searchSubstring.search( fileReader, "aboba");
+        FileReader fileReader = new FileReader("./src/test/resources/test.txt");
+        ArrayList<Integer> list1 = searchSubstring.search(fileReader, "aboba");
         assertEquals(list, list1);
     }
 
     @Test
-    void generateBigTest() throws IOException {
+    void generateBigTestString() throws IOException {
         File file = new File("./src/test/resources/test1.txt");
         file.createNewFile();
         BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file));
         bufferedWriter.append("abbaboba");
-        for(int i =0; i < Integer.MAX_VALUE; i++){
-            bufferedWriter.append( (char) ('c' + (i % 24)));
+        for (int i = 0; i < Integer.MAX_VALUE; i++) {
+            bufferedWriter.append((char) ('c' + (i % 24)));
         }
         ArrayList<Integer> list = new ArrayList<>();
         list.add(3);
-        ArrayList<Integer> list1 =  searchSubstring.search(
+        ArrayList<Integer> list1 = searchSubstring.search(
                 "./src/test/resources/test1.txt", "aboba");
+        assertEquals(list, list1);
+        file.delete();
+    }
+
+    @Test
+    void generateBigTestFile() throws IOException {
+        File file = new File("./src/test/resources/test1.txt");
+        file.createNewFile();
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file));
+        bufferedWriter.append("abbaboba");
+        for (int i = 0; i < Integer.MAX_VALUE; i++) {
+            bufferedWriter.append((char) ('c' + (i % 24)));
+        }
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(3);
+        BufferedReader fileReader =
+                new BufferedReader(new FileReader("./src/test/resources/test1.txt"));
+
+        ArrayList<Integer> list1 = searchSubstring.search(fileReader, "aboba");
         assertEquals(list, list1);
         file.delete();
     }
