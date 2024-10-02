651eddd23af72d700767e62bf2ac8839ee655882
yura-poj <yura-poj@users.noreply.github.com>
2024-10-02 04:18:28 +0000

    Deploying to gh-pages from @ yura-poj/OOP@8b14d7d5e08a185e2973f2d0d182a71e7bc871ce 🚀

---
+++ Task_1_4_1/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"ru.nsu.pozhidaev","l":"RecordBook"},{"p":"ru.nsu.pozhidaev","l":"Student"},{"p":"ru.nsu.pozhidaev","l":"Subject"}]
