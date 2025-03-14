62c1e8e72f41be052099b2bfb2f6e658932a79d8
Yurii Pozhidaev <y.pozhidaev@g.nsu.ru>
2025-03-14 11:58:15 +0700

    add snake test

--- Task_2_3_1/src/test/java/ru/nsu/pozhidaev/SnakeTest.java
+++ Task_2_3_1/src/test/java/ru/nsu/pozhidaev/SnakeTest.java
@@ -1,27 +1,69 @@
 package ru.nsu.pozhidaev;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 class SnakeTest {
+    Snake snake;
+    @BeforeEach
+    void setUp() {
+        snake = new Snake(5,5);
+      }
 
     @Test
     void setDirection() {
+        snake.setDirection(Action.UP);
+        snake.move();
+        assertEquals(5, snake.getHead().getX());
+        assertEquals(4, snake.getHead().getY());
+        snake.setDirection(Action.RIGHT);
+        snake.move();
+        assertEquals(6, snake.getHead().getX());
+        assertEquals(4, snake.getHead().getY());
+        snake.setDirection(Action.DOWN);
+        snake.move();
+        assertEquals(6, snake.getHead().getX());
+        assertEquals(5, snake.getHead().getY());
+        snake.setDirection(Action.LEFT);
+        snake.move();
+        assertEquals(5, snake.getHead().getX());
+        assertEquals(5, snake.getHead().getY());
       }
 
     @Test
     void move() {
+        assertEquals(5, snake.getHead().getX());
+        assertEquals(5, snake.getHead().getY());
+        snake.move();
+        assertEquals(4, snake.getHead().getX());
+        assertEquals(5, snake.getHead().getY());
       }
 
     @Test
     void getBody() {
+        assertEquals(3, snake.getBody().size());
       }
 
     @Test
     void getHead() {
+        assertEquals(5, snake.getHead().getX());
+        assertEquals(5, snake.getHead().getY());
       }
 
     @Test
     void lunch() {
-      }
+        List<SnakePart> body = snake.getBody();
+        SnakePart lastPart = body.get(body.size() - 1);
+        int x = lastPart.getX();
+        int y = lastPart.getY();
+        snake.lunch();
+        snake.move();
+        body = snake.getBody();
+        SnakePart newLastPart = body.get(body.size() - 1);
+        assertEquals(x, newLastPart.getX());
+        assertEquals(y, newLastPart.getY());
+    }
 }
