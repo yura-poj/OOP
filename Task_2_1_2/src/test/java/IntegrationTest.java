33f59006845ab34d7635ba4102f3b46566c35316
Yurii Pozhidaev <y.pozhidaev@g.nsu.ru>
2025-04-11 15:14:25 +0700

    working tests

--- Task_2_1_2/src/test/java/IntegrationTest.java
+++ Task_2_1_2/src/test/java/IntegrationTest.java
@@ -3,57 +3,48 @@ import org.junit.jupiter.api.Assertions;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.Network;
 
-import java.time.Duration;
-
 public class IntegrationTest {
 
     private static final Network NETWORK = Network.newNetwork();
 
-    GenericContainer<?> worker1 = new GenericContainer<>("task_2_1_2-worker1:latest")
-            .withNetwork(NETWORK)
-            .withNetworkAliases("worker1")
-            .withExposedPorts(5005)
-            .withCommand("java", "-cp", "build/classes/java/main", "ru.nsu.pozhidaev.worker.Main");
-    GenericContainer<?> worker2 = new GenericContainer<>("task_2_1_2-worker2:latest")
-            .withNetwork(NETWORK)
-            .withNetworkAliases("worker2")
-            .withExposedPorts(5005)
-            .withCommand("java", "-cp", "build/classes/java/main", "ru.nsu.pozhidaev.worker.Main");
-
+    @Test
     public void testManagerFindsWorkers() throws Exception {
         try (
+                GenericContainer<?> worker1 = new GenericContainer<>("task_2_1_2-worker1:latest")
+                        .withNetwork(NETWORK)
+                        .withNetworkAliases("worker1")
+                        .withCommand("java", "-cp", "build/classes/java/main",
+                                "ru.nsu.pozhidaev.worker.Main");
+                GenericContainer<?> worker2 = new GenericContainer<>("task_2_1_2-worker2:latest")
+                        .withNetwork(NETWORK)
+                        .withNetworkAliases("worker2")
+                        .withCommand("java", "-cp", "build/classes/java/main",
+                                "ru.nsu.pozhidaev.worker.Main");
                 GenericContainer<?> manager = new GenericContainer<>("task_2_1_2-manager:latest")
                         .withNetwork(NETWORK)
                         .withNetworkAliases("manager")
-                        .withExposedPorts(5005)
-                        .withStartupTimeout(Duration.ofSeconds(20))
                         .withCommand("java", "-cp", "build/classes/java/main",
                                 "ru.nsu.pozhidaev.manager.Main",
                                 "2", "3", "4", "5", "6", "7", "8", "9", "10", "11")
         ) {
             worker1.start();
             worker2.start();
-            while (!worker1.isRunning()) {
-                Thread.sleep(100);
-            }
             manager.start();
 
             Thread.sleep(5000);
 
             String managerLogs = manager.getLogs();
-            System.out.println("Manager container logs:");
+            String worker1Logs = worker1.getLogs();
+            String worker2Logs = worker2.getLogs();
+
+            System.out.println("Manager logs:");
             System.out.println(managerLogs);
+            System.out.println("\nWorker1 logs:");
+            System.out.println(worker1Logs);
+            System.out.println("\nWorker2 logs:");
+            System.out.println(worker2Logs);
 
             Assertions.assertTrue(managerLogs.contains("Find worker"), "Manager didn't find any workers");
         }
-    }
-
-    @Test
-    public void workerLog() throws InterruptedException {
-        worker1.start();
-        String workerLogs = worker1.getLogs();
-        System.out.println(workerLogs);
-        Thread.sleep(2000);
-        assert workerLogs.contains("Listening");
     }
 }
