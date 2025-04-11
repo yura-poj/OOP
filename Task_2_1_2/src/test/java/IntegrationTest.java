32b4a7138a816f9a8de83e5829f1b88378778c48
Yurii Pozhidaev <y.pozhidaev@g.nsu.ru>
2025-04-12 01:09:12 +0700

    SUDAAAAAAAA

--- Task_2_1_2/src/test/java/IntegrationTest.java
+++ Task_2_1_2/src/test/java/IntegrationTest.java
@@ -110,4 +110,60 @@ public class IntegrationTest {
             Assertions.assertTrue(worker2Logs.contains("get task: [102, 153"), "Worker didn't receive part");
         }
     }
+
+    @Test
+    public void testManagerAfterManager() throws Exception {
+        try (
+                GenericContainer<?> worker1 = new GenericContainer<>("task_2_1_2-worker1:latest")
+                        .withNetwork(NETWORK)
+                        .withNetworkAliases("worker1")
+                        .withCommand("java", "-cp", "build/classes/java/main",
+                                "ru.nsu.pozhidaev.worker.Main");
+                GenericContainer<?> worker2 = new GenericContainer<>("task_2_1_2-worker2:latest")
+                        .withNetwork(NETWORK)
+                        .withNetworkAliases("worker2")
+                        .withCommand("java", "-cp", "build/classes/java/main",
+                                "ru.nsu.pozhidaev.worker.Main");
+                GenericContainer<?> manager = new GenericContainer<>("task_2_1_2-manager:latest")
+                        .withNetwork(NETWORK)
+                        .withNetworkAliases("manager")
+                        .withCommand("java", "-cp", "build/classes/java/main",
+                                "ru.nsu.pozhidaev.manager.Main",
+                                "2", "3", "4", "5", "6", "7", "8", "9", "10", "11");
+                GenericContainer<?> manager2 = new GenericContainer<>("task_2_1_2-manager:latest")
+                        .withNetwork(NETWORK)
+                .withNetworkAliases("manager")
+                .withCommand("java", "-cp", "build/classes/java/main",
+                        "ru.nsu.pozhidaev.manager.Main",
+                        "2", "3", "4", "5", "6", "7", "8", "9", "10", "11")
+        ) {
+            worker1.start();
+            worker2.start();
+            manager.start();
+
+            Thread.sleep(20000);
+
+            String managerLogs = manager.getLogs();
+            String worker1Logs = worker1.getLogs();
+            String worker2Logs = worker2.getLogs();
+
+            System.out.println("Manager logs:");
+            System.out.println(managerLogs);
+            System.out.println("\nWorker1 logs:");
+            System.out.println(worker1Logs);
+            System.out.println("\nWorker2 logs:");
+            System.out.println(worker2Logs);
+
+            Assertions.assertTrue(managerLogs.contains("Find worker"), "Manager didn't find any workers");
+            Assertions.assertTrue(managerLogs.contains("true"), "Manager didn't find the answer");
+
+            manager2.start();
+
+            Thread.sleep(20000);
+            String manager2Logs = manager2.getLogs();
+            
+            Assertions.assertTrue(manager2Logs.contains("Find worker"), "Manager didn't find any workers");
+            Assertions.assertTrue(manager2Logs.contains("true"), "Manager didn't find the answer");
+        }
+    }
 }
