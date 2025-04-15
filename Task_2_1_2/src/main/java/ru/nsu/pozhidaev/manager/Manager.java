6300b277af60eac984e1c841b3f8a13150175e64
Yurii Pozhidaev <y.pozhidaev@g.nsu.ru>
2025-04-15 23:18:16 +0700

    fix mistake

--- Task_2_1_2/src/main/java/ru/nsu/pozhidaev/manager/Manager.java
+++ Task_2_1_2/src/main/java/ru/nsu/pozhidaev/manager/Manager.java
@@ -249,7 +249,7 @@ public class Manager {
         }
 
         private void lostWorker() {
-            synchronized (manager.getLock()) {
+            synchronized (manager.getUnfinishedLock()) {
                 manager.getUnfinished().addAll(getTask());
             }
             synchronized (manager.getWorkersLock()) {
